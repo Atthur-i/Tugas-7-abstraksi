be5719fd878e485dd1d818ee096104649d2cb6a4
Atthur-i <61936700+Atthur-i@users.noreply.github.com>
2020-06-11 20:38:52 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Tugas_abstraction.Abstrak;
+using Tugas_abstraction.inter;
+
+namespace Tugas_abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vertebrata vertebrata;
+
+            vertebrata = new Mamalia();
+            vertebrata.Bernapas();
+
+            Console.WriteLine();
+            vertebrata = new Pisces();
+            vertebrata.Bernapas();
+
+            Console.WriteLine();
+            vertebrata = new Amphibia();
+            vertebrata.Bernapas();
+
+            Console.WriteLine();
+            IVertebrata ivertebrata;
+
+            ivertebrata = new Reptilia();
+            ivertebrata.Bernapas();
+
+            Console.WriteLine();
+            ivertebrata = new Aves();
+            ivertebrata.Bernapas();
+
+            Console.ReadKey();
+        }
+    }
+}
